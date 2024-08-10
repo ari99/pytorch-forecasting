5f4557d668327a5aa4581d7efaf44c6a92e6004d
ari99 <>
2024-08-10 12:13:21 -0400

    formatting

--- examples/stallion.py
+++ examples/stallion.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import pickle
 import warnings
 import os
-# needs to be before other imports 
+# needs to be before other imports
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 import lightning.pytorch as pl
